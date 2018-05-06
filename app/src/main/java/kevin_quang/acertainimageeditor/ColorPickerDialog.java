ecc4ef6109027fc4e795d11fbdcaed1dd4f2677e
Quang Duong <duongquang1@gmail.com>
2018-05-05 20:03:58 -0500

    Color picker + shoe icon

---
+++ app/src/main/java/kevin_quang/acertainimageeditor/ColorPickerDialog.java
@@ -0,0 +1,67 @@
+package kevin_quang.acertainimageeditor;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.larswerkman.holocolorpicker.ColorPicker;
+import com.larswerkman.holocolorpicker.OpacityBar;
+import com.larswerkman.holocolorpicker.SaturationBar;
+import com.larswerkman.holocolorpicker.ValueBar;
+
+public class ColorPickerDialog extends DialogFragment {
+
+    private EditDisplaySurfaceView editDisplaySurfaceView;
+
+    /**
+     * Create a new instance of MyDialogFragment, providing "num"
+     * as an argument.
+     */
+    public static ColorPickerDialog newInstance() {
+        ColorPickerDialog f = new ColorPickerDialog();
+        Bundle args = new Bundle();
+        f.setArguments(args);
+
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int style = DialogFragment.STYLE_NORMAL, theme = 0;
+        setStyle(style, theme);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.color_picker_dialog, container, false);
+
+        final ColorPicker picker = (ColorPicker) v.findViewById(R.id.picker);
+        OpacityBar opacityBar = (OpacityBar) v.findViewById(R.id.opacitybar);
+        SaturationBar saturationBar = (SaturationBar) v.findViewById(R.id.saturationbar);
+        ValueBar valueBar = (ValueBar) v.findViewById(R.id.valuebar);
+
+        picker.addOpacityBar(opacityBar);
+        picker.addSaturationBar(saturationBar);
+        picker.addValueBar(valueBar);
+
+        picker.setOnColorChangedListener(new ColorPicker.OnColorChangedListener() {
+            @Override
+            public void onColorChanged(int color) {
+                // TODO: Select color
+            }
+        });
+
+        return v;
+    }
+}
